7fd4a326a4f44708812b33e9350e415060f4ba75
Gowtham21062004 <142236401+Gowtham21062004@users.noreply.github.com>
2024-08-28 09:32:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,94 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='chess'>
+      <div className='row'>
+        <div className='box'><span>&#9814;</span></div>
+        <div className=' box black'><span>&#9822;</span></div>
+        <div className='box'><span>&#9821;</span></div>
+        <div className='box black'><span>&#9819;</span></div>
+        <div className='box'><span>&#9818;</span></div>
+        <div className='box black'><span>&#9821;</span></div>
+        <div className='box'><span>&#9822;</span></div>
+        <div className='box black'><span>&#9814;</span></div>
+      </div>
+      <div className='row'>
+        <div className='box black'><span>&#9823;</span></div>
+        <div className='box'><span>&#9823;</span></div>
+        <div className='box black'><span>&#9823;</span></div>
+        <div className='box'><span>&#9823;</span></div>
+        <div className='box black'><span>&#9823;</span></div>
+        <div className='box'><span>&#9823;</span></div>
+        <div className='box black'><span>&#9823;</span></div>
+        <div className='box'><span>&#9823;</span></div>
+      </div>
+      <div className='row'>
+        <div className='box'></div>
+        <div className=' box black'></div>
+        <div className='box'></div>
+        <div className='box black'></div>
+        <div className='box'></div>
+        <div className='box black'></div>
+        <div className='box'></div>
+        <div className='box black'></div>
+      </div>
+      <div className='row'>
+        <div className='box black'></div>
+        <div className='box'></div>
+        <div className='box black'></div>
+        <div className='box'></div>
+        <div className='box black'></div>
+        <div className='box'></div>
+        <div className='box black'></div>
+        <div className='box'></div>
+      </div>
+      <div className='row'>
+        <div className='box'></div>
+        <div className=' box black'></div>
+        <div className='box'></div>
+        <div className='box black'></div>
+        <div className='box'></div>
+        <div className='box black'></div>
+        <div className='box'></div>
+        <div className='box black'></div>
+      </div>
+      <div className='row'>
+        <div className='box black'></div>
+        <div className='box'></div>
+        <div className='box black'></div>
+        <div className='box'></div>
+        <div className='box black'></div>
+        <div className='box'></div>
+        <div className='box black'></div>
+        <div className='box'></div>
+      </div>
+      <div className='row'>
+        <div className='box'><span>&#9817;</span></div>
+        <div className=' box black'><span>&#9817;</span></div>
+        <div className='box'><span>&#9817;</span></div>
+        <div className='box black'><span>&#9817;</span></div>
+        <div className='box'><span>&#9817;</span></div>
+        <div className='box black'><span>&#9817;</span></div>
+        <div className='box'><span>&#9817;</span></div>
+        <div className='box black'><span>&#9817;</span></div>
+      </div>
+      <div className='row'>
+        <div className='box black'><span>&#9814;</span></div>
+        <div className='box'><span>&#9816;</span></div>
+        <div className='box black'><span>&#9815;</span></div>
+        <div className='box'><span>&#9813;</span></div>
+        <div className='box black'><span>&#9812;</span></div>
+        <div className='box'><span>&#9815;</span></div>
+        <div className='box black'><span>&#9816;</span></div>
+        <div className='box'><span>&#9814;</span></div>
+      </div>
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
